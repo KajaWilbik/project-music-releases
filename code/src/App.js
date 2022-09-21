b4bfff646d8b855d0ef8ca592dd371abefb6b755
KajaWilbik <wilbik.kaja@gmail.com>
2022-09-21 12:33:12 +0200

    Maria's code

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import data from './data.json';
+import { ArtistName } from './components/Artistname'
+import { SongTitle } from './components/SongTitle'  //showing in html
 
 console.log(data);
 
+/* Possible options: <PhotoAlbum layout="rows" photos={image} />*/
+/*Possible options: <img className="album-image" src={props.image} alt={props.name} />*/
+
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <>
+    
+    {data.albums.items.map(item => {
+      return <ArtistName key = {data.id} data = {item} text = "Test"/> //declaring a property called data ,item is the value
+      })}
+    </>
+    
   );
 }
